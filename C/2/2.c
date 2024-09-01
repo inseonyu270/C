19a4d9efd916edd96779d81ed48670d38b7aeb1d
유인선 <124290448+inseonyu270@users.noreply.github.com>
2024-09-01 21:36:52 +0900

    Update 2.c

--- C/2/2.c
+++ C/2/2.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main(void)
+{
+	int number1 = 1;
+	int number2 = 2;
+	int number3 = 3, number4 = 4;	
+	printf("%d\n", number1);
+	printf("%d\n", number2);
+	printf("%d\n", number3);
+	printf("%d\n", number4);
+
+	return 0;
+}
